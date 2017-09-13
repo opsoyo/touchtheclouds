0975fd266dbabedabae7f89e34ff5d2f894edb78
opsoyo <3497372+opsoyo@users.noreply.github.com>
2017-09-13 10:28:05 -0700

    Update gettracks.py

--- gettracks.py
+++ gettracks.py
@@ -18,36 +18,43 @@ class ydl_logger(object):
 
 def ydl_hook(d):
     if d['status'] == 'finished':
-        print('Download complete...{}'.format(d['filename']))
+        print('\r\n*****\r\nYTDL>> Download complete...{}\r\n*****'.format(d['filename']))
 
 with open('profiles.json') as data_file:
     data = json.load(data_file)
 
-for char in data:
-    for profile in data[char]:
-        sys.stdout.write("\x1b]2;Working Profile: {}\x07".format(profile))
-        ydl_opts = {
-            'quiet': False,
-            'no_warnings': False,
-            'ignoreerrors': True,
-            'simulate': False,
-            'format': 'best',
-            'outtmpl': 'soundcloud/{}/%(webpage_url_basename)s/%(title)s.%(id)s.%(ext)s'.format(profile),
-            'continuedl': True,
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-            'writeinfojson': True,
-            'restrictfilenames': False,
-            'nooverwrites': True,
-            'embed_thumbnail': True,
-            'nocheckcertificate': False,
-            'prefer_insecure': False,
-            'socket_timeout': '600',
-            'retries': 10,
-            #'external_downloader': 'axel',
-            #'external_downloader_args': ['--num-connections=16'],
-            'logger': ydl_logger(),
-            'progress_hooks': [ydl_hook],
-        }
+    for char in data:
+        for profile in data[char]:
+            #print('Working profile: {}'.format(profile))
+            sys.stdout.write("\x1b]2;Working Profile: {}\x07".format(profile))
+            ydl_opts = {
+                #'consoletitle': 'Working Profile: %(uploader)s',
+                'quiet': False,
+                'no_warnings': False,
+                'ignoreerrors': False,
+                'verbose': False,
+                'simulate': False,
+                'continuedl': True,
+                'restrictfilenames': True,
+                'nooverwrites': True,
+                'format': 'best',
+                'outtmpl': 'soundcloud/%(uploader)s/%(webpage_url_basename)s/%(title)s.%(id)s/%(title)s.%(id)s.%(ext)s',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+                'writeinfojson': True,
+                'writedescription': True,
+                'write_all_thumbnails': True,
+                'nocheckcertificate': False,
+                'prefer_insecure': False,
+                'socket_timeout': '600',
+                'retries': 10,
+                #'debug_printtraffic': True,
+                #'dump_pages': True,
+                #'call_home': True,
+                'external_downloader': 'aria2c',
+                'external_downloader_args': ['-c','-j','3','-x','3','-s','3','-k','1M'],
+                'logger': ydl_logger(),
+                'progress_hooks': [ydl_hook],
+            }
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            results = ydl.extract_info('https://soundcloud.com/{}'.format(profile))
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                results = ydl.download(['https://soundcloud.com/{}'.format(profile)])
